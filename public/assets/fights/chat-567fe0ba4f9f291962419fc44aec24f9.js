19b90f94dd94798aa0d5585f117d80d09a152242
yihsiaol <yihsiaol@me.com>
2014-06-18 11:54:29 -0400

    Local asset precompile

---
+++ public/assets/fights/chat-567fe0ba4f9f291962419fc44aec24f9.js
@@ -0,0 +1,34 @@
+$(function() {
+  var dispatcher = new WebSocketRails('localhost:3000/websocket'); // pull host dynamically
+  var userName = ''
+
+  dispatcher.on_open = function(connID) {
+    dispatcher.subscribe('lobby');
+    userName = connID.connection_id;
+
+    dispatcher.bind("chat.new_user", function(new_user){
+      $('#users').append('<li>' + new_user + '<li>');
+    });
+    console.log('Connection has been established: ', connID);
+
+    dispatcher.trigger('chat.new_user', userName);
+  };
+
+  dispatcher.bind("chat.current_users", function(current_users){
+    debugger
+  });
+
+  dispatcher.bind("chat.message", function(message) {
+    $("#chat").append('<li>' + message.userName + ' :    ' + message.message + '</li>');
+  });
+
+  $("#form").on("submit", function( evt ) {
+    evt.preventDefault();
+    var message = {
+                    message: $("#message").val(),
+                    userName: userName
+                  };
+    dispatcher.trigger("chat.message", message);
+    this.reset();
+  });
+});
