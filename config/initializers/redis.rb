95cd18aee6ae7b944de739a84d8d0094ced92d12
yihsiaol <yihsiaol@me.com>
2014-06-18 17:49:06 -0400

    Flush Redis to ensure clean state on server start

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,6 @@
 # Use Redis Cloud add-on for Heroku on production, else connect to localhost on port 6379
 settings = ENV["REDISCLOUD_URL"] ? { url: ENV["REDISCLOUD_URL"] } : { host: "localhost", port: 6379 }
 $redis = Redis.new settings
+
+# Ensure Redis is in a clean state
+$redis.flushdb
