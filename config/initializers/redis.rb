a472d8accca7985804d1bd7d47c6ead32492ce5d
yihsiaol <yihsiaol@me.com>
2014-06-18 12:01:18 -0400

    Fix Redis deployment

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 # Use Redis Cloud add-on for Heroku on production, else connect to localhost on port 6379
-settings = ENV["REDISCLOUD_URL"] ? ENV["REDISCLOUD_URL"] : { host: "localhost", port: 6379 }
+settings = ENV["REDISCLOUD_URL"] ? { url: ENV["REDISCLOUD_URL"] } : { host: "localhost", port: 6379 }
 $redis = Redis.new settings
