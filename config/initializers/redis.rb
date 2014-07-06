9e140faf61a416aa9fd2d440dd30459676439d27
yihsiaol <yihsiaol@me.com>
2014-07-06 11:15:34 -0400

    Change Redis add-on to Redistogo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 # Use Redis Cloud add-on for Heroku on production, else connect to localhost on port 6379
-settings = ENV["REDISCLOUD_URL"] ? { url: ENV["REDISCLOUD_URL"] } : { host: "localhost", port: 6379 }
+settings = ENV["REDISTOGO_URL"] ? { url: ENV["REDISTOGO_URL"] } : { host: "localhost", port: 6379 }
 $redis = Redis.new settings
 
 # Ensure Redis is in a clean state
